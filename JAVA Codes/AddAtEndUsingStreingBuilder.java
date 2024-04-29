fa3ba41360b4516f55e7536e3e01ad421bd0316b
SUDARSHAN9171 <145355498+SUDARSHAN9171@users.noreply.github.com>
2024-04-29 22:42:48 +0530

    Added about

---
+++ JAVA Codes/AddAtEndUsingStreingBuilder.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class AddAtEndUsingStreingBuilder {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("tony");
+        System.out.println(sb);
+        sb.append(" stark");
+        System.out.println(sb);
+    }
+}
