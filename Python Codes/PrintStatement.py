91fbeea07c2f4742ea89b6cbc9271590c834003a
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-09 12:51:39 +0530

    Added Code

---
+++ Python Codes/PrintStatement.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print('Hi')
+print("""This is sudarshan""")
+print("This is a demonstration","of how to use print statement in python")
+age = 18
+print(type(age))
+name = 'Sudarshan'
+print(type(name))
+bol = True
+print(type(bol))
+fraction = 98.9
+print(type(fraction))
