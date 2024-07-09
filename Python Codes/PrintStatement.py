274305d08b007ec4d9ad13913de94402e984008d
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-09 13:31:17 +0530

    Update Code

--- Python Codes/PrintStatement.py
+++ Python Codes/PrintStatement.py
@@ -1,8 +1,11 @@
+#just a print statement
 print("Hello World")
 print('Hi')
 print("""This is sudarshan""")
 print("This is a demonstration","of how to use print statement in python")
+#declearing a variable  
 age = 18
+#print the data type
 print(type(age))
 name = 'Sudarshan'
 print(type(name))
