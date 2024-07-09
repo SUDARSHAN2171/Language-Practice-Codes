d42ddb3102169dd06fd01439ad970c343dae761c
SUDARSHAN2171 <sudarshanjadhav9171@gmail.com>
2024-07-09 13:41:39 +0530

    Update Code

--- Python Codes/TakeInput.py
+++ Python Codes/TakeInput.py
@@ -1,2 +1,7 @@
+#taking input and displaying
+name=input("Enter your name : ")
 age=input("Enter your age : ")
+decimal_value =input("Enter any decimal value : ")
+print("My name is :",name)
 print("My age is :",age)
+print("Decimal value :",(decimal_value))
